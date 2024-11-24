102ce37b4f7eaefee5abd39cf99e6a93f336259f
nipunpuri <nipun.puri@gmail.com>
2024-11-24 14:32:01 -0500

    another try

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -27,7 +27,6 @@ unzip rawdata.zip
 
 mkdir data
 
-
 # 2. Move the ./rawdata directory to ./data/raw
 
 mkdir data/raw
@@ -38,7 +37,10 @@ mv rawdata/* data/raw/
 ls data/raw
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
-mkdir -p data/processed/server_logs data/processed/user_logs data/processed/event_logs
+mkdir data/processed
+mkdir data/processed/server_logs
+mkdir data/processed/user_logs
+mkdir data/processed/event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 cp data/raw/server* data/processed/server_logs
